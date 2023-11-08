f4b453b28ab051a277d00c9170a861ec56e5e54c
sosuisen <hidekazu.kubota@gmail.com>
2023-11-09 02:03:04 +0900

    feat: use File System API

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,79 @@
 import { useEffect, useState } from 'react'
 import { format } from 'date-fns';
-
 import './App.css'
+import { PageEntity } from '@logseq/libs/dist/LSPlugin.user';
 // import { BlockEntity, IDatom } from '@logseq/libs/dist/LSPlugin.user';
 
+const getLastUpdatedTime = async (page: PageEntity, preferredDateFormat: string, handle: FileSystemDirectoryHandle) => {
+  // Encode characters that are not allowed in windows file name.
+  const fileName = page.originalName
+    .replace(/</g, '%3C')
+    .replace(/>/g, '%3E')
+    .replace(/:/g, '%3A')
+    .replace(/"/g, '%22')
+    .replace(/\//g, '___')
+    .replace(/\\/g, '%5C')
+    .replace(/\|/g, '%7C')
+    .replace(/\?/g, '%3F')
+    .replace(/\*/g, '%2A');
 
+  // Cannot get from subdirectory.
+  // const path = `pages/${fileName}.md`;
+  const path = `${fileName}.md`;
+  // console.log(path);
+  const fileHandle = await handle.getFileHandle(path).catch(() => {
+    // Empty page is not saved as a file.
+    console.log(`Failed to get file handle: ${path}`);
+    // console.log(page);
+    return null;
+  });
+  if (!fileHandle) return '';
+  const file = await fileHandle.getFile();
 
-const getLastUpdatedTime = async (fileName: string, preferredDateFormat: string) => {
-  let res;
-  let attempts = 0;
-  const maxRetries = 7;
+  // console.log(file.lastModified);
+  const date = new Date(file.lastModified);
 
-  while (attempts < maxRetries) {
-    res = await logseq.Git.execCommand(['log', '-1', '--format="%ad"', '--', `pages/${fileName}.md`]).catch(e => {
-      console.log(`Attempt ${attempts + 1} for ${fileName} failed:`, e);
-      attempts++;
-      if (attempts >= maxRetries) {
-        console.log(`Failed after ${maxRetries} attempts: ${fileName}`);
-      }
-    });
-    if (res) break;
-  }
-  if (!res) return '';
+  const dateUserFormat = format(date, preferredDateFormat);
+  // console.log(dateUserFormat);
+  return {
+    formattedDate: dateUserFormat,
+    time: `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
+    unixTime: date.getTime(),
+  };
 
-  // Wed Nov 8 02:37:35 2023 +0900
-  const regex = /(\w{3}) (\w{3}) (\d{1,2}) (\d{2}):(\d{2}):(\d{2}) (\d{4}) (\+\d{4})/;
-  const match = res!.stdout.match(regex);
-  if (match) {
-    // remove timezone
-    const date = new Date(`${match[1]} ${match[2]} ${match[1]} ${match[3]} ${match[4]}:${match[5]}:${match[6]} ${match[7]}`);
-    const dateUserFormat = format(date, preferredDateFormat);
-    return {
-      formattedDate: dateUserFormat,
-      time: `${match[4]}:${match[5]}:${match[6]}`,
-      unixTime: date.getTime(),
-    };
-  }
-  return '';
+  /*
+    let res;
+    let attempts = 0;
+    const maxRetries = 7;
+  
+    while (attempts < maxRetries) {
+  
+      res = await logseq.Git.execCommand(['log', '-1', '--format="%ad"', '--', `pages/${fileName}.md`]).catch(e => {
+        console.log(`Attempt ${attempts + 1} for ${fileName} failed:`, e);
+        attempts++;
+        if (attempts >= maxRetries) {
+          console.log(`Failed after ${maxRetries} attempts: ${fileName}`);
+        }
+      });
+      if (res) break;
+    }
+    if (!res) return '';
+  
+    // Wed Nov 8 02:37:35 2023 +0900
+    const regex = /(\w{3}) (\w{3}) (\d{1,2}) (\d{2}):(\d{2}):(\d{2}) (\d{4}) (\+\d{4})/;
+    const match = res!.stdout.match(regex);
+    if (match) {
+      // remove timezone
+      const date = new Date(`${match[1]} ${match[2]} ${match[1]} ${match[3]} ${match[4]}:${match[5]}:${match[6]} ${match[7]}`);
+      const dateUserFormat = format(date, preferredDateFormat);
+      return {
+        formattedDate: dateUserFormat,
+        time: `${match[4]}:${match[5]}:${match[6]}`,
+        unixTime: date.getTime(),
+      };
+    }
+    return '';
+    */
 };
 
 type Box = {
@@ -50,23 +87,23 @@ type Box = {
 };
 
 function App() {
+  const [dirHandle, setDirHandle] = useState<FileSystemDirectoryHandle>();
+
   const [boxes, setBoxes] = useState<Box[]>([]);
-  const [currentGraph, setCurrentGraph] = useState<string>('');
-  console.log(currentGraph);
+  // const [currentGraph, setCurrentGraph] = useState<string>('');
+
   useEffect(() => {
     const fetchData = async () => {
-      const { preferredDateFormat, currentGraph } = await logseq.App.getUserConfigs();
-      const arr = currentGraph.split('/');
-      setCurrentGraph(arr[arr.length - 1]);
-      console.log('fetching...');
+      const { preferredDateFormat } = await logseq.App.getUserConfigs();
+      // const arr = currentGraph.split('/');
+      // setCurrentGraph(arr[arr.length - 1]);
 
       const pages = await logseq.Editor.getAllPages();
       if (!pages) return [];
       for (const page of pages) {
         if (page['journal?']) continue;
 
-        const updatedTime = await getLastUpdatedTime(page.originalName.replace(/\//g, '___'), preferredDateFormat);
-
+        const updatedTime = await getLastUpdatedTime(page, preferredDateFormat, dirHandle!);
         if (updatedTime === '') continue;
 
         const box = {
@@ -74,25 +111,20 @@ function App() {
           updatedTime,
           uuid: page.uuid,
         };
-        console.log(box);
+        // console.log(box);
         setBoxes(boxes => [...boxes, box].sort((a, b) => b.updatedTime.unixTime - a.updatedTime.unixTime))
       }
-      /*
-      logseq.DB.onChanged(async (changes: {
-        blocks: BlockEntity[];
-        txData: IDatom[];
-        txMeta?: {
-            outlinerOp: string;
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            [key: string]: any;
-        };
-    }) => {
-        console.log(changes);
-      });
-      */
+    };
+
+    if (dirHandle) {
+      fetchData();
     }
-    fetchData();
-  }, []);
+  }, [dirHandle]);
+
+  const openDirectoryPicker = async () => {
+    const handle = await window.showDirectoryPicker();
+    setDirHandle(handle);
+  };
 
   const boxOnClick = async (box: Box, e: React.MouseEvent<HTMLDivElement>) => {
     if (e.shiftKey) {
@@ -116,7 +148,7 @@ function App() {
         {box.originalName}
       </div>
       <div className='box-date'>
-        {box.updatedTime.formattedDate}<br/>
+        {box.updatedTime.formattedDate}<br />
         {box.updatedTime.time}
       </div>
     </div>
@@ -126,11 +158,15 @@ function App() {
     <>
       <div className='control'>
         <span className='cardbox-title'>CardBox</span>
+        <button className='open-btn' onClick={() => openDirectoryPicker()}>
+          pagesを選択
+        </button>
+        { dirHandle === undefined && <div className='open-btn-label'>Logseqのグラフの保存先にあるpagesディレクトリを選択してください。</div> }
         <button className='close-btn' onClick={() => logseq.hideMainUI()}>
           close
         </button>
       </div>
-      <div className='box-tile'>
+      <div className='tile'>
         {boxElements}
       </div>
     </>
