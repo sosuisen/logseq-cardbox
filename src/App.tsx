7bece1ebe9b2417b03f1eaa2dbc8edb1e36c12a2
sosuisen <hidekazu.kubota@gmail.com>
2023-11-08 19:38:21 +0900

    feat: can jump to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,107 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { format } from 'date-fns';
+
 import './App.css'
 
-const getLastUpdatedTime = async (fileName: string) => {
-  const res = await logseq.Git.execCommand(['log', '-1', '--format="%ad"', '--', `pages/${fileName}.md`]);
+
+
+const getLastUpdatedTime = async (fileName: string, preferredDateFormat: string) => {
+  let res;
+  let attempts = 0;
+  const maxRetries = 7;
+
+  while (attempts < maxRetries) {
+    res = await logseq.Git.execCommand(['log', '-1', '--format="%ad"', '--', `pages/${fileName}.md`]).catch(e => {
+      console.log(`Attempt ${attempts + 1} for ${fileName} failed:`, e);
+      attempts++;
+      if (attempts >= maxRetries) {
+        console.log(`Failed after ${maxRetries} attempts: ${fileName}`);
+      }
+    });
+    if (res) break;
+  }
+  if (!res) return "";
+
   // console.log(res);
   // Wed Nov 8 02:37:35 2023 +0900
   const regex = /(\w{3}) (\w{3}) (\d{1,2}) (\d{2}):(\d{2}):(\d{2}) (\d{4}) (\+\d{4})/;
-  const match = res.stdout.match(regex);
+  const match = res!.stdout.match(regex);
   if (match) {
-    return `${match[1]} ${match[2]} ${match[1]} ${match[3]} ${match[4]}:${match[5]}:${match[6]} ${match[7]}`;
+    // remove timezone
+    const date = new Date(`${match[1]} ${match[2]} ${match[1]} ${match[3]} ${match[4]}:${match[5]}:${match[6]} ${match[7]}`);
+    const dateUserFormat = format(date, preferredDateFormat);
+    return {
+      formatted: dateUserFormat,
+      unixTime: date.getTime(),
+    };
   }
   return "";
 };
 
+type Box = {
+  originalName: string;
+  updatedTime: {
+    formatted: string;
+    unixTime: number;
+  };
+  uuid: string;
+};
+
 function App() {
-  // const [count, setCount] = useState(0)
+  const [boxes, setBoxes] = useState<Box[]>([]);
+  const [currentGraph, setCurrentGraph] = useState<string>("");
 
   useEffect(() => {
     const fetchData = async () => {
+      const { preferredDateFormat, currentGraph } = await logseq.App.getUserConfigs();
+      const arr = currentGraph.split("/");
+      setCurrentGraph(arr[arr.length - 1]);
+      console.log("fetching...");
+
       const pages = await logseq.Editor.getAllPages();
       if (!pages) return [];
-      for(const page of pages) {
+      for (const page of pages) {
         if (page["journal?"]) continue;
 
-        const name = page.originalName.replace(/\//g, '___');
-        const updatedTime = await getLastUpdatedTime(name);
+        const updatedTime = await getLastUpdatedTime(page.originalName.replace(/\//g, '___'), preferredDateFormat);
 
         if (updatedTime === "") continue;
 
         const box = {
-          name: page.originalName,
-          updatedTime, 
+          originalName: page.originalName,
+          updatedTime,
           uuid: page.uuid,
         };
         console.log(box);
+        setBoxes(boxes => [...boxes, box].sort((a, b) => b.updatedTime.unixTime - a.updatedTime.unixTime))
       }
-//      const results = await Promise.all(pages.map(page =>));
-
-//      console.log(results);
-      /*
-        return {
-          updatedAt: new Date(page.updatedAt).toUTCString(),
-          name,
-        };
-      });
-      const results = pages.sort((a, b) => {
-        return a.updatedAt > b.updatedAt ? -1 : 1
-      })
-    */
     }
     fetchData();
-
-
   }, []);
 
-return (
-  <>
-    <button onClick={() => logseq.hideMainUI()}>
-      close
-    </button>
+  const boxOnClick = () => {
+    setTimeout(() => { logseq.hideMainUI() }, 500);
+  };
 
-  </>
-)
+  const boxElements = boxes.map((box: Box) => (
+    <a href={`logseq://graph/${currentGraph}?page=${box.uuid}`}>
+      <div onClick={() => boxOnClick()}>
+        {box.originalName}
+        <span>
+          {box.updatedTime.formatted}
+        </span>
+      </div>
+    </a>
+  ));
+
+  return (
+    <>
+      <button onClick={() => logseq.hideMainUI()}>
+        close
+      </button>
+      {boxElements}
+    </>
+  )
 }
 
 export default App
