64705d4f32c0c6a76a93143368f09133f8b7de9f
sosuisen <hidekazu.kubota@gmail.com>
2023-11-10 19:51:01 +0900

    fix: add db.ts

---
+++ src/db.ts
@@ -0,0 +1,24 @@
+import Dexie, { Table } from 'dexie';
+
+export interface Box {
+  name: string; // originalName in Logseq db
+  time: number; // Unix time
+  summary: string[];
+}
+
+export class CardBoxDexie extends Dexie {
+  box!: Table<Box>; 
+
+  constructor(dbName: string) {
+    super(dbName);
+    this.version(1).stores({
+      box: '&name, time' // name is the primary key and followings are indexed props
+    });
+  }
+}
+
+export let db: CardBoxDexie;
+
+export const setDB = (dbName: string) => {
+    db = new CardBoxDexie(dbName);
+};
