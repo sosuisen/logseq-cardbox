fb9e1edccdad0baa054ef9e5d24a107387aaf440
sosuisen <hidekazu.kubota@gmail.com>
2023-11-10 15:32:59 +0900

    feat: add i18n

---
+++ src/i18n/configs.ts
@@ -0,0 +1,27 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+// 言語jsonファイルのimport
+import translation_en from "./en.json";
+import translation_ja from "./ja.json";
+
+const resources = {
+    ja: {
+        translation: translation_ja
+    },
+    en: {
+        translation: translation_en
+    }
+};
+
+i18n
+    .use(initReactI18next)
+    .init({
+        resources,
+        fallbackLng: "en",
+        interpolation: {
+            escapeValue: false,
+        }
+    });
+
+export default i18n;
